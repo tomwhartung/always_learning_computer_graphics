d60fee01caa8948a108f831304c9b91040cd7fca
Tom W Hartung <tomwhartung@gmail.com>
2023-06-26 15:35:11 -0600

    Updated mdn/projects/3-canvas_with_react_hooks/src/Canvas.jsx in accordance with Step 3 of the Reference, and added a couple of comments.

--- mdn/projects/3-canvas_with_react_hooks/src/Canvas.jsx
+++ mdn/projects/3-canvas_with_react_hooks/src/Canvas.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 const Canvas = () => {
-  const canvas = React.useRef();
+  const canvas = React.useRef();   // Added useRef in Step 2
+
+  // ADDED useEffect in Step 3:
+  React.useEffect(() => {
+    const context = canvas.current.getContext('2d');
+  });
 
   return (
     <canvas
-      ref={canvas}
+      ref={canvas}   // Added ref in Step 2
       width="100"
       height="100"
     />
