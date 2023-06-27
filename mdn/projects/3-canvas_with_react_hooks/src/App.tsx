e810946abe1c6c0558e7458743763101ef038091
Tom W Hartung <tomwhartung@gmail.com>
2023-06-27 15:03:27 -0600

    Added a div containing our new Canvas element to mdn/projects/3-canvas_with_react_hooks/src/App.tsx , along with a few constants and a draw function with code from Project 1.  This file still shows an problem in VSCode but it runs anyway.  I tried to fix the problem but was unsuccessful; for details see subsection 2.6.2.2. [Try to] Fix the Error ... of mdn/notes/3-canvas_with_react_hooks.md .

--- mdn/projects/3-canvas_with_react_hooks/src/App.tsx
+++ mdn/projects/3-canvas_with_react_hooks/src/App.tsx
@@ -8,13 +8,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import './Canvas.jsx'
+import Canvas from './Canvas.jsx'
+
+const width = 333;
+const height = 333;
+const draw = (context: CanvasRenderingContext2D) => {
+  // Paint it black
+  context.fillStyle = "rgb(0, 0, 0)";
+  context.fillRect(0, 0, width, height);
+
+  // Add an opaque red rectangle at (50,50) that is 100 px wide and 150 px tall
+  context.fillStyle = "rgb(255, 0, 0)";
+  context.fillRect(50, 50, 100, 150);
+  // Add an opaque green rectangle at (75,75) that is 100 px wide and 100 px tall
+  context.fillStyle = "rgb(0, 255, 0)";
+  context.fillRect(75, 75, 100, 100);
+
+  // Add a translucent purple rectangle at (25,100) that is 175 px wide and 150 px tall
+  context.fillStyle = "rgba(255, 0, 255, 0.75)";
+  context.fillRect(25, 100, 175, 50);
+};
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+        <Canvas draw={draw} width={width} height={height} />
+      </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
