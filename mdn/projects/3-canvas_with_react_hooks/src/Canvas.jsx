a98564a24a90d3d3b8579ff1afeb55677e130788
Tom W Hartung <tomwhartung@gmail.com>
2023-06-26 19:10:53 -0600

    Updated mdn/projects/3-canvas_with_react_hooks/src/Canvas.jsx to use props for the width and height of the canvas element.

--- mdn/projects/3-canvas_with_react_hooks/src/Canvas.jsx
+++ mdn/projects/3-canvas_with_react_hooks/src/Canvas.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 // import PropTypes from 'prop-types'; // Added in Step 4
 
-const Canvas = ( {draw} ) => {     // Changed in Step 4
-  const canvas = React.useRef();   // Added useRef in Step 2
+const Canvas = ( {draw, width, height} ) => {   // Changed in Steps 4 and 5
+  const canvas = React.useRef();                // Added useRef in Step 2
 
   // Added useEffect in Step 3:
   React.useEffect(() => {
@@ -13,9 +13,9 @@ const Canvas = ( {draw} ) => {     // Changed in Step 4
 
   return (
     <canvas
-      ref={canvas}   // Added ref in Step 2
-      width="100"
-      height="100"
+      ref={canvas}       // Added ref in Step 2
+      width={width}      // Added width prop in Step 5
+      height={height}    // Added height prop in Step 5
     />
   )
 };
